56431d4cbf957d402487da356c43826d1ced7521
yasu-swift <sp4c72q9@icloud.com>
2021-09-14 21:07:02 +0900

    課題修正2

--- exercise/03/calc.php
+++ exercise/03/calc.php
@@ -4,8 +4,6 @@ $num1 = $_GET['num1'];
 $num2 = $_GET['num2'];
 $operator = $_GET['operator'];
 
-// echo $num1 , $num2, $operator;
-
 if ($operator == 'addition') {
     $result = $num1 + $num2;
     echo "${num1} + ${num2} = ${result}";
@@ -21,20 +19,3 @@ if ($operator == 'addition') {
 } else {
     echo '正しい演算子を指定して下さい';
 }
-
-// function ans($num1, $num2, $operator, $result)
-// {
-//     if ($operator == 'addition') {
-//             $result = $num1 + $num2;
-//         }elseif ($operator =='subtraction'){
-//             $result = $num1 - $num2;
-//         }elseif ($operator == 'multiplication'){
-//             $result = $num1 * $num2;
-//         }elseif ($operator == 'division'){
-//             $result = $num1 / $num2;
-//         }else{
-//             $result = '正しい演算子を指定して下さい';
-//         }
-//         return $result;
-// };
-
--- exercise/06/index.php
+++ exercise/06/index.php
@@ -1,13 +1,10 @@
 <?php
 
-use function PHPUnit\Framework\at;
-
 class Car
 {   
     private $car_name;
     private $car_num;
     private $car_colour;
-
 
     public function __construct($car_name, $car_num, $car_colour,)
     {
@@ -35,23 +32,9 @@ class Car
         return '車の車種:' . $this->car_name . '、車体番号:' . $this->car_num .
                 '、カラー:' . $this->car_colour. 'です。';
     }
-
-    // public function setCarName($car_name)
-    // {
-    //     $this->car_name = $car_name;
-    // }
-
-    // public function setCarNum($car_num)
-    // {
-    //     $this->car_num = $car_num;
-    // }
-    // public function setCarColour($car_colour)
-    // {
-    //     $this->car_colour = $car_colour;
-    // }
-
     
 }
+
 //車1台目表示
 $car1 = new car('ランクル', 100, 'brack');
 
